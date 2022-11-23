e35022184fa515264deee58316de5c8dc4e76f82
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-23 10:36:39 +0300

    Debugging Network Requests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,22 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
+  def create
+    # Debugging Server Errors
+    # Movi instead of Movie
+    #  movie = Movi.create(movie_params) => POST http://localhost:4000/movies
+  # 500 (Internal Server Error)
+  # NameError (uninitialized constant MoviesController::Movi):
+  # app/controllers/movies_controller.rb:10:in 'create'
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+  private
+
+  def movie_params
+
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
 
 end
